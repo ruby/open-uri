d09f5d80571f5b5e8e3a1c1b740388f1cebabe11
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-04-02 15:50:23 +0900

    bundle gem open-uri

---
+++ lib/open/uri.rb
@@ -0,0 +1,8 @@
+require "open/uri/version"
+
+module Open
+  module Uri
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/open/uri/version.rb
@@ -0,0 +1,5 @@
+module Open
+  module Uri
+    VERSION = "0.1.0"
+  end
+end
